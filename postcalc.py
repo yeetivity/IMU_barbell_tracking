c0d58e64060e271395e1b47dd7774219894506ac
Mathias Olsson <matholss@kth.se>
2023-10-01 14:44:20 +0200

    kalman filter added
    tried to implement from the lab2 in CM2000

--- postcalc.py
+++ postcalc.py
@@ -1,5 +1,8 @@
 import json
 from tkinter import Tk, filedialog
+from kalmanFilter import KalmanFilter
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Open file dialog
 root = Tk()
@@ -30,5 +33,55 @@ def sliding_window(elements, window_size):
 resultx = sliding_window(x, 10)
 resulty = sliding_window(y, 10)
 resultz = sliding_window(z, 10)
-print(resultx)
 
+# Set up Kalman filter 
+dt = 1/52 # sampling time
+u_x = 0 # assuming zero acceleration
+u_y = 0
+std_acc = 0.1 # tune this
+x_std_meas = 0.5 # tune this
+y_std_meas = 0.5 # tune this
+
+self = None
+
+kf = KalmanFilter(dt, u_x, u_y, std_acc, x_std_meas, y_std_meas)
+
+# Filter each axis
+filtered_x = []
+for x_meas in resultx:
+  x = kf.update(np.matrix([[x_meas]]))
+  filtered_x.append(x[0,0])
+
+filtered_y = []  
+for y_meas in resulty:
+  y = kf.update(np.matrix([[y_meas]]))
+  filtered_y.append(y[1,0])
+
+# Calculate velocity from filtered acceleration data using numerical integration
+def integrate_acceleration(filtered_acceleration, dt):
+    velocity = [0]  # Initial velocity is 0
+    for i in range(1, len(filtered_acceleration)):
+        # Use trapezoidal rule for numerical integration
+        delta_velocity = (filtered_acceleration[i] + filtered_acceleration[i - 1]) / 2 * dt
+        velocity.append(velocity[-1] + delta_velocity)
+    return velocity
+
+# Assuming dt is the sampling time (which is 1/52 in your case)
+sampling_time = 1/52
+
+# Calculate velocity for filtered x and y acceleration data
+velocity_x = integrate_acceleration(filtered_x, sampling_time)
+velocity_y = integrate_acceleration(filtered_y, sampling_time)
+
+# Now, velocity_x and velocity_y contain the calculated velocities from filtered x and y acceleration data respectively
+
+# Plot the velocities
+plt.figure(figsize=(10, 6))
+plt.plot(velocity_x, label='Velocity X')
+plt.plot(velocity_y, label='Velocity Y')
+plt.xlabel('Time Steps')
+plt.ylabel('Velocity')
+plt.title('Velocity Calculated from Filtered Acceleration Data')
+plt.legend()
+plt.grid(True)
+plt.show()
