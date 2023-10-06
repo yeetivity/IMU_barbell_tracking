b1a12c70ab2445073c3d6da9732e1f6aed86245f
Mathias Olsson <matholss@kth.se>
2023-10-06 19:25:38 +0200

    kalman filter implementation
    kind of working now

--- plotacc.py
+++ plotacc.py
@@ -18,13 +18,28 @@ x = data['accX']
 y = data['accY']
 z = data['accZ']
 
+# Create subplots
 fig, axs = plt.subplots(3)
-axs[0].plot(x)
-axs[0].set_title('Acc X')
-axs[1].plot(y)
-axs[1].set_title('Acc Y')
-axs[2].plot(z)
-axs[2].set_title('Acc Z')
 
+# Set the line color to red and increase y-axis size and range for each subplot
+axs[0].plot(x, color='red')
+axs[0].set_title('Acceleration x direction')
+axs[0].tick_params(axis='both', labelsize=12)  # Increase both x and y-axis tick font size
+axs[0].set_xlabel('dT [1/52*s]', fontsize=10)  # X-axis label with smaller font size
+axs[0].set_ylabel('[$m/s^2$]', fontsize=10)  # Y-axis label with LaTeX formatting
+
+axs[1].plot(y, color='blue')
+axs[1].set_title('Acceleration y direction')
+axs[1].tick_params(axis='both', labelsize=12)  # Increase both x and y-axis tick font size
+axs[1].set_xlabel('dT [1/52*s]', fontsize=10)  # X-axis label with smaller font size
+axs[1].set_ylabel('[$m/s^2$]', fontsize=10)  # Y-axis label with LaTeX formatting
+
+axs[2].plot(z, color='green')
+axs[2].set_title('Acceleration z direction')
+axs[2].tick_params(axis='both', labelsize=12)  # Increase both x and y-axis tick font size
+axs[2].set_xlabel('dT [1/52*s]', fontsize=10)  # X-axis label with smaller font size
+axs[2].set_ylabel('[$m/s^2$]', fontsize=10)  # Y-axis label with LaTeX formatting
+
+# Adjust layout and display the plot
 plt.tight_layout()
 plt.show()
--- postcalc.py
+++ postcalc.py
@@ -17,7 +17,7 @@ with open(file_path) as f:
 # Assuming your JSON structure contains 'x' and 'y' arrays, modify as needed
 x = data['accX']
 y = data['accY']
-z = data['accZ']
+#z = data['accZ']
 
 plt.subplot(2, 1, 1)
 plt.plot(x, label='Raw Acceleration X')
@@ -47,43 +47,60 @@ def sliding_window(elements, window_size):
         result.append(window_mean)
     return result
 
-resultx = sliding_window(x, 10)
-resulty = sliding_window(y, 10)
-resultz = sliding_window(z, 10)
+#resultx = sliding_window(x, 10)
+#resulty = sliding_window(y, 10)
+#resultz = sliding_window(z, 10)
 
 # Set up Kalman filter (2D x,y (not z!!))
-dt = 1/52 # sampling time
+dt = 1/52 # sample time
 u_x = 0 # assuming zero acceleration
 u_y = 0
 std_acc = 0.1 # tune this
-x_std_meas = 0.5 # tune this
-y_std_meas = 0.5 # tune this
+x_std_meas = 0.1 # tune this
+y_std_meas = 0.05 # tune this
 
-self = None
+#initiates the kalman filter
+kf = KalmanFilter(dt, u_x, u_y, std_acc, x_std_meas, y_std_meas)
 
-#kf = KalmanFilter(dt, u_x, u_y, std_acc, x_std_meas, y_std_meas)
+x_filtered = []
+y_filtered = []
 
+# Make x and y 1D arrays (maybe not needed)
+x = np.squeeze(x)
+y = np.squeeze(y)
+
+# Kalman filtering loop
+for i in range(len(x)):
+    # Predict  
+    x_est = kf.predict()
+    
+    # Update
+    state = kf.update(np.array([[x[i]], [y[i]]]))
+    
+    # Extract x and y   
+    x_filtered.append(state[0, 0])
+    y_filtered.append(state[1, 0])
+
+#kalman filters are defined for linear systems
 # Filter each axis
-#filtered_x = []
-#for x_meas in resultx:
-#  x = kf.update(np.matrix([[x_meas]]))
-#  filtered_x.append(x[0,0])
+  
+# Plot filtered vs raw data  
+plt.plot(x, label='x raw')
+plt.plot(x_filtered, label='x filtered')
+plt.plot(y, label='y raw')
+plt.plot(y_filtered, label='y filtered')
+plt.legend()
+plt.show()
 
-filtered_x = x
-
-#filtered_y = []  
-#for y_meas in resulty:
-#  y = kf.update(np.matrix([[y_meas]]))
-#  filtered_y.append(y[1,0])
-
-filtered_y = y
+#filtered_y = y
 g = 9.82
 
 def calcStartAngle(yAcc, g):
     
-    # Calculate the average of the first 5 values in yAcc
-    avg_yAcc = np.mean(yAcc[:5])
-
+    # Calculate the average of the first 10 values in yAcc
+    avg_yAcc = np.mean(yAcc[:10])
+    print(avg_yAcc)
+    
     startangle = np.arccos(avg_yAcc/-g) * 180 / np.pi
     print(startangle)
     
@@ -95,7 +112,7 @@ def calcStartAngle(yAcc, g):
     print(startangle)
     return startangle
 
-startangle = calcStartAngle(y, g)
+startangle = calcStartAngle(y_filtered, g)
 threshold = 0.1
 
 #takes a acceleration vector and loop index as input 
@@ -115,7 +132,7 @@ def detect_turning_point(acc, i):
  
 #checks if the device is moving
 def checkmovement(i):
-    if ((filtered_x[i] > threshold and filtered_x[i] < -threshold) or (filtered_y[i] > threshold and filtered_y[i] < -threshold)):
+    if ((x_filtered[i] > threshold and x_filtered[i] < -threshold) or (y_filtered[i] > threshold and y_filtered[i] < -threshold)):
         return True
     else:
         return False
@@ -124,12 +141,17 @@ def checkmovement(i):
 def integrate_acceleration(filtered_acceleration, dt):
     velocity = [0]  # Initial velocity is 0
     for i in range(1, len(filtered_acceleration)):
-        if (detect_turning_point(y,i)):
+        if detect_turning_point(filtered_acceleration, i):
             velocity.append(0)
         else:
             # Use trapezoidal rule for numerical integration
             delta_velocity = (filtered_acceleration[i] + filtered_acceleration[i - 1]) / 2 * dt
-            velocity.append(velocity[-1] + delta_velocity)
+
+            # Adjust velocity based on the sign of acceleration
+            if filtered_acceleration[i] >= 0:
+                velocity.append(velocity[-1] + delta_velocity)
+            else:
+                velocity.append(velocity[-1] - delta_velocity)
     return velocity
 
 # Assuming dt is the sampling time (which is 1/52 in your case)
@@ -137,8 +159,8 @@ sampling_time = 1/52
 
 # Calculate velocity for filtered x and y acceleration data
 
-velocity_x = integrate_acceleration(x, sampling_time)
-velocity_y = integrate_acceleration(y, sampling_time)
+velocity_x = integrate_acceleration(x_filtered, sampling_time)
+velocity_y = integrate_acceleration(y_filtered, sampling_time)
 
 # Now, velocity_x and velocity_y contain the calculated velocities from filtered x and y acceleration data respectively
 
